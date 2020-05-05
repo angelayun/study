d4d001b4d111edfbd63138ce00bddacb42cb18ab
李云花 <liyunhua_angela@163.com>
2020-05-05 11:54:14 +0800

    动态加载脚本async默认是true

---
+++ theory/react-dedux-demo/public/test1.js
@@ -0,0 +1,4 @@
+console.log(1)
+var obj = {
+    foo: 'foo'
+}
--- theory/react-dedux-demo/src/App.js
+++ theory/react-dedux-demo/src/App.js
@@ -27,11 +27,28 @@ const mapDispatchToProps = dispatch => {
 }
 
 class App extends React.Component {
+    dynamicLoad () {
+        var getReadyForEditor = () => {
+            // console.log(obj.foo)
+        }
+
+        var editorJs = document.createElement("script")
+        editorJs.src = "./test1.js"
+        editorJs.async = false
+        document.body.appendChild(editorJs)
+
+        var editorJs2 = document.createElement("script")
+        editorJs2.src = "./test2.js"
+        editorJs2.async = false
+        editorJs2.onload = getReadyForEditor
+        document.body.appendChild(editorJs2)
+    }
     render () {
         return (
             <div className="App">
                 {this.props.count}
                 <button onClick={() => this.props.addAsyncCount()}>增加</button>
+                <button onClick={() => this.dynamicLoad()}>动态加载脚本</button>
             </div>
         );
     }
